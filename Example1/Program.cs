2bd12386efddc03d748b6353cd55617b1a8a5958
Furkan Türkal <furkan.turkal@hotmail.com>
2017-07-22 22:40:24 +0300

    Small fixes

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,11 +1,15 @@
 ﻿using EasySilkroadSecurityApi;
+using EasySilkroadSecurityApi.Extensions;
+using EasySilkroadSecurityApi.SSA;
 
 namespace Example1 {
     class Program {
         static void Main(string[] args) {
 
             EasySSASettings s = new EasySSASettings();
-            
+
+            Packet t = new Packet();
+            t.DODebug().fullPosition
 
             EasySSA ssa = new EasySSA();
 
