bc56aa7514632a73f84cd422395e9cd2dfb2fa29
Furkan Türkal <furkan.turkal@hotmail.com>
2017-07-22 21:29:05 +0300

    Added Example1 Project

---
+++ Example1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
